c445395e6ec81336cb9af379ac1b9181ea661f4b
PC2\User <ahmad52585258@gmail.com>
2022-10-22 22:03:49 +0300

    printf of diffrent data

---
+++ HackerRank/Basic data type.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int num1;
+    long num2;
+    char c ;
+    float num3 ;
+    double num4;
+    cin>>num1>>num2>>c>>num3>>num4;
+    printf("%d \n%ld \n%c \n%0.3f \n%0.9lf",num1,num2,c,num3,num4);
+    return 0;
+}
